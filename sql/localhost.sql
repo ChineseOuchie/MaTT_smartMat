407c9a2463ca620de75608abb913f27962f9816a
jeroenvanoers <jeroen.vanoers@ictmbo.nl>
2018-03-19 15:14:13 +0100

    database deleted infix

--- sql/localhost.sql
+++ sql/localhost.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: Mar 05, 2018 at 04:20 PM
+-- Generation Time: Mar 19, 2018 at 03:13 PM
 -- Server version: 5.5.42
 -- PHP Version: 7.0.8
 
@@ -34,18 +34,15 @@ CREATE TABLE `loginGebruiker` (
   `username` varchar(30) NOT NULL,
   `password` varchar(30) NOT NULL,
   `email` varchar(30) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `loginGebruiker`
 --
 
 INSERT INTO `loginGebruiker` (`idloginGebruiker`, `username`, `password`, `email`) VALUES
-(4, 'Yoga', 'KAAS', 'yoga@matt.nl'),
-(5, 'kaas', 'yoga', 'kaas@yoga.com'),
-(7, 'adn', 'kaasjes123', 'adn@crossbowjoey.nl'),
-(8, 'adn', 'kaasjes123', 'adn@crossbowjoey.nl'),
-(9, 'yogamatt420', 'kipjes123', 'yoga@matt.nl');
+(9, 'yogamatt420', 'kipjes123', 'yoga@matt.nl'),
+(10, 'jeroentje2000', 'kipjes123', 'jeroen.vanoers@ictmbo.nl');
 
 -- --------------------------------------------------------
 
@@ -58,7 +55,6 @@ CREATE TABLE `userprofile` (
   `iduserprofile` int(11) NOT NULL,
   `username` varchar(30) NOT NULL,
   `firstname` varchar(30) NOT NULL,
-  `infix` varchar(15) DEFAULT NULL,
   `sirname` varchar(30) NOT NULL,
   `age` int(11) NOT NULL,
   `weight` int(11) NOT NULL,
@@ -73,14 +69,15 @@ CREATE TABLE `userprofile` (
   `yogaHalfmoon` int(30) NOT NULL,
   `yogaHatha` int(30) NOT NULL,
   `image` varchar(200) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `userprofile`
 --
 
-INSERT INTO `userprofile` (`iduserprofile`, `username`, `firstname`, `infix`, `sirname`, `age`, `weight`, `score`, `achFirstExercise`, `achFirstFinish`, `achTouchZen`, `danBallet`, `danSalsa`, `danStreet`, `yogaEssentials`, `yogaHalfmoon`, `yogaHatha`, `image`) VALUES
-(6, 'yogamatt420', 'Yoga', '', 'Matt', 26, 79, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL);
+INSERT INTO `userprofile` (`iduserprofile`, `username`, `firstname`, `sirname`, `age`, `weight`, `score`, `achFirstExercise`, `achFirstFinish`, `achTouchZen`, `danBallet`, `danSalsa`, `danStreet`, `yogaEssentials`, `yogaHalfmoon`, `yogaHatha`, `image`) VALUES
+(6, 'yogamatt420', 'Yoga', 'Matt', 26, 79, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'yogamatt420.jpeg'),
+(10, 'jeroentje2000', 'Jeroen', 'Oers', 17, 60, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'jeroentje2000');
 
 --
 -- Indexes for dumped tables
@@ -106,12 +103,50 @@ ALTER TABLE `userprofile`
 -- AUTO_INCREMENT for table `loginGebruiker`
 --
 ALTER TABLE `loginGebruiker`
-  MODIFY `idloginGebruiker` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+  MODIFY `idloginGebruiker` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT for table `userprofile`
 --
 ALTER TABLE `userprofile`
-  MODIFY `iduserprofile` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `iduserprofile` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;--
+-- Database: `smikkelsnel`
+--
+CREATE DATABASE IF NOT EXISTS `smikkelsnel` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `smikkelsnel`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `idusers` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`idusers`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `idusers` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
