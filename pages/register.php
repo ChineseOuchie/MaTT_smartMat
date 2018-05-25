c026d9d566e6613a569427f55de76da99f97586a
Joey <joeylau1998@gmail.com>
2018-05-25 13:17:47 +0200

    Register page

--- pages/register.php
+++ pages/register.php
@@ -40,25 +40,31 @@
 <div id="container">
     <img src="../img/logo.png" id="logo">
     <form method="post" id="registerForm">
-        <input id="firstname" type="text" name="firstname" class="registerInput" required placeholder="Firstname">
-        <input id="sirname" type="text" name="sirname" class="registerInput" required placeholder="Surname">
-        <input id="email" type="email" name="email" placeholder="email">
-        <input id="password" type="password" name="password" class="registerInput" required placeholder="Password">
-        <input id="rePassword" type="password" name="rePassword" class="registerInput" required placeholder="rePassword">
-        <div id="registerMsg"><?php echo $msg?></div>
-        <label>
-            <input id="male" type="radio" class="registerInput" name="gender" value="male">
-            <img src="../img/male.png">
-        </label>
-        <label>
-            <input id="female" type="radio" class="registerInput" name="gender" value="female">
-            <img src="../img/female.png">
-        </label>
+        <div id="sides">
+            <div id="registerSide1">
+                <input id="firstname" type="text" name="firstname" class="registerInput" required placeholder="Firstname">
+                <input id="sirname" type="text" name="sirname" class="registerInput" required placeholder="Surname">
+                <input id="email" type="email" name="email" class="registerInput" placeholder="email">
+                <input id="password" type="password" name="password" class="registerInput" required placeholder="Password">
+                <input id="rePassword" type="password" name="rePassword" class="registerInput" required placeholder="rePassword">
+            </div>
+            <div id="registerSide2">
+                <div id="registerMsg"><?php echo $msg?></div>
+                <label>
+                        <input id="male" type="radio" class="registerInput" name="gender" value="male">
+                        <img src="../img/male.png">
+                </label>
+                <label>
+                    <input id="female" type="radio" class="registerInput" name="gender" value="female">
+                    <img src="../img/female.png">
+                </label>
 
-        <input id="age" type="number" name="age" class="registerInput" required placeholder="Age">
-        <input id="weight" type="number" name="weight" class="registerInput" required placeholder="weight in KG">
+                <input id="age" type="number" name="age" class="registerInput" required placeholder="Age">
+                <input id="weight" type="number" name="weight" class="registerInput" required placeholder="weight in KG">
+                <input type="submit" name="submit" value="next" id="registerSubmit">
 
-        <input type="submit" name="submit" value="next">
+            </div>
+        </div>
     </form>
     <video autoplay muted loop id="bgVideo">
         <source src="../video/running.mp4" type="video/mp4">
