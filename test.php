bc5ec0cf00fa66cd7bd0ec148e169f88ac1032d4
jeroenvanoers <jeroen.vanoers@ictmbo.nl>
2018-06-04 16:22:41 +0200

    test Goals

--- test.php
+++ test.php
@@ -1,28 +1,128 @@
-<?php
-    if (isset($_POST['upload'])) {
-    // Get image name
-        $image = $_FILES['image']['name'];
-        $base = basename($image);
-        $target = "img/{$base}";
-        echo $target;
-    }
-?>
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Chivo" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/test.css">
 </head>
 <body>
-<form method="POST" action="test.php" enctype="multipart/form-data">
-    <input type="hidden" name="size" value="1000000">
-    <div>
-        <input type="file" name="image">
+
+<div class="content">
+    <div class="inContent">
+        <h1>Goals</h1>
+        <hr>
+
+        <!--GOALS KOMEN HIER-->
+        <div id="goalOutput">
+
+        </div>
+        <!--GOALS KOMEN HIER-->
+
     </div>
-    <button type="submit" name="upload">POST</button>
-</form>
+    <div id="myBtn">+</div>
+</div>
+
+<div id="myModal" class="modal">
+
+    <div class="modal-content">
+        <span class="close">&times;</span>
+
+        <form action="#" method="post">
+            <label class="formtitle">Category</label>
+            <select id="select" name="category" onchange="selectchange()">
+                <option disabled selected> -- select an option -- </option>
+                <option value="weight">Weight loss</option>
+                <option value="exercise">Exercise</option>
+                <option value="calories">Calories</option>
+            </select><br><br>
+
+            <div id="weightlossForm">
+                <label>Amount of Kilograms you want to lose</label>
+                <input type="number"><br>
+
+                <label>Days to finish goal</label>
+                <input type="number"><br><br>
+
+                <input type="submit" value="Submit Goal">
+            </div>
+
+            <div id="exerciseForm">
+                <label>Amount of days</label>
+                <input type="number"><br>
+
+                <label>Per</label>
+                <select name="exerciseSelect">
+                    <option disabled selected>-- Period of time --</option>
+                    <option>Day</option>
+                    <option>Week</option>
+                    <option>Month</option>
+                </select><br><br>
+
+                <input type="submit" value="Submit Goal">
+            </div>
+
+            <div id="caloriesForm">
+                <label>Amount of calories</label>
+                <input type="number"><br>
+
+                <label>In how many days</label>
+                <input type="number"><br><br>
+
+                <input type="submit" value="Submit Goal">
+            </div>
+        </form>
+    </div>
+
+</div>
+
+<script>
+    const modal = document.getElementById('myModal');
+
+    const btn = document.getElementById("myBtn");
+
+    const span = document.getElementsByClassName("close")[0];
+
+    btn.addEventListener("click", function () {
+        modal.style.display = "block";
+    });
+
+    span.addEventListener("click", function () {
+        modal.style.display = "none";
+    });
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+
+    const weightloss = document.getElementById("weightlossForm");
+
+    const exercise = document.getElementById("exerciseForm");
+
+    const calories = document.getElementById("caloriesForm");
+
+    function selectchange() {
+        if(document.getElementById("select").value === "weight") {
+            weightloss.style.display = "block";
+            exercise.style.display = "none";
+            calories.style.display = "none";
+        }
+
+        if(document.getElementById("select").value === "exercise") {
+            exercise.style.display = "block";
+            weightloss.style.display = "none";
+            calories.style.display = "none";
+        }
+
+        if(document.getElementById("select").value === "calories") {
+            calories.style.display = "block";
+            exercise.style.display = "none";
+            weightloss.style.display = "none";
+        }
+    }
+</script>
+
 </body>
 </html>
+
