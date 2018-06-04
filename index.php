2f8dde100a1e681e74844cebf146126edb22c67a
Joey <joeylau1998@gmail.com>
2018-06-04 15:36:58 +0200

    Mostly done

--- index.php
+++ index.php
@@ -17,12 +17,65 @@
 <!--    <a href="pages/register.php">Register</a>-->
 <!--    <a href="pages/logout.php">Logout</a>-->
 
-    <img src="img/gear.png" alt="" class="dashboardIcon">
-    <img src="img/trophy.png" alt="" class="dashboardIcon">
+    <a href="" class="dashboardIcon"><img src="img/gear.png" alt="" ></a>
+    <a href="" class="dashboardIcon"><img src="img/trophy.png" alt="" ></a>
+    <a href="" class="dashboardIcon"><img src="img/profile.png" alt="" ></a>
     <div id="dashboardItem">
         <div id="goal" class="item"> Goals</div>
-        <div id="health" class="item"> Health</div>
-        <div id="workout" class="item">Workout Videos</div>
+        <div id="health" class="item">
+            <p class="title">Health</p>
+            <div class="health">
+                <img src="img/grafiek1.png">
+                <div class="info">
+                    <h3>Daily Activity</h3>
+                    <p>Find more information of your daily activity here</p>
+                    <a href="">More...</a>
+                </div>
+            </div>
+            <div class="health">
+                <img src="img/grafiek2.png">
+                <div class="info">
+                    <h3>Calories</h3>
+                    <p>Find more information of your calories here.</p>
+                    <a href="">More...</a>
+                </div>
+            </div>
+            <div class="health">
+                <img src="img/grafiek3.png">
+                <div class="info">
+                    <h3>Weight</h3>
+                    <p>Find more information of your weight here.</p>
+                    <a href="">More...</a>
+                </div>
+            </div>
+        </div>
+        <div id="workout" class="item">
+            <p class="title">Workout Videos</p>
+            <div class="workout">
+                <img src="img/grafiek1.png">
+                <div class="info">
+                    <h3>Dance</h3>
+                    <p>Find more of our dance videos here</p>
+                    <a href="">More...</a>
+                </div>
+            </div>
+            <div class="workout">
+                <img src="img/grafiek1.png">
+                <div class="info">
+                    <h3>Fitness</h3>
+                    <p>Find more of our fitness videos here</p>
+                    <a href="">More...</a>
+                </div>
+            </div>
+            <div class="workout">
+                <img src="img/grafiek2.png">
+                <div class="info">
+                    <h3>Yoga</h3>
+                    <p>Find more of our yoga videos here</p>
+                    <a href="">More...</a>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 <video autoplay muted loop id="bgVideo">
