1afca25f63b7cc04ab97141f7876825af51e077a
Joey <joeylau1998@gmail.com>
2018-06-11 14:39:04 +0200

    sql

--- sql/localhost.sql
+++ sql/localhost.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.10
--- http://www.phpmyadmin.net
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Gegenereerd op: 28 mei 2018 om 14:20
--- Serverversie: 5.5.42
--- PHP-versie: 7.0.8
+-- Generation Time: Jun 11, 2018 at 12:38 PM
+-- Server version: 5.6.34-log
+-- PHP Version: 7.1.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,33 +21,74 @@ SET time_zone = "+00:00";
 --
 -- Database: `matt`
 --
+DROP DATABASE IF EXISTS `matt`;
 CREATE DATABASE IF NOT EXISTS `matt` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `matt`;
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `loginGebruiker`
+-- Table structure for table `achievements`
 --
 
-DROP TABLE IF EXISTS `loginGebruiker`;
-CREATE TABLE `loginGebruiker` (
-  `idloginGebruiker` int(11) NOT NULL,
-  `password` varchar(30) NOT NULL,
-  `email` varchar(30) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
-
---
--- Gegevens worden geëxporteerd voor tabel `loginGebruiker`
---
-
-INSERT INTO `loginGebruiker` (`idloginGebruiker`, `password`, `email`) VALUES
-(9, 'kipjes123', 'yoga@matt.nl');
+DROP TABLE IF EXISTS `achievements`;
+CREATE TABLE `achievements` (
+  `idachievements` int(11) NOT NULL,
+  `firstExercise` varchar(30) NOT NULL,
+  `firstFinish` varchar(30) NOT NULL,
+  `touchZen` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `userprofile`
+-- Table structure for table `danceclasses`
+--
+
+DROP TABLE IF EXISTS `danceclasses`;
+CREATE TABLE `danceclasses` (
+  `iddanceclasses` int(11) NOT NULL,
+  `streetDance` varchar(30) NOT NULL,
+  `balletDance` varchar(30) NOT NULL,
+  `salsaDance` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `images`
+--
+
+DROP TABLE IF EXISTS `images`;
+CREATE TABLE `images` (
+  `idimages` int(11) NOT NULL,
+  `image` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `images`
+--
+
+INSERT INTO `images` (`idimages`, `image`) VALUES
+(1, 'placeholder');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logingebruiker`
+--
+
+DROP TABLE IF EXISTS `logingebruiker`;
+CREATE TABLE `logingebruiker` (
+  `idloginGebruiker` int(11) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userprofile`
 --
 
 DROP TABLE IF EXISTS `userprofile`;
@@ -67,50 +110,97 @@ CREATE TABLE `userprofile` (
   `yogaHalfmoon` int(30) NOT NULL,
   `yogaHatha` int(30) NOT NULL,
   `image` varchar(200) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
--- Gegevens worden geëxporteerd voor tabel `userprofile`
+-- Table structure for table `yogaclasses`
 --
 
-INSERT INTO `userprofile` (`iduserprofile`, `firstname`, `sirname`, `age`, `gender`, `weight`, `score`, `achFirstExercise`, `achFirstFinish`, `achTouchZen`, `danBallet`, `danSalsa`, `danStreet`, `yogaEssentials`, `yogaHalfmoon`, `yogaHatha`, `image`) VALUES
-(6, 'Yoga', 'Matt', 26, '', 79, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'yogamatt420.png'),
-(10, 'Jeroen', 'Oers', 17, '', 60, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'jeroentje2000.png'),
-(11, 'Paco', 'Lee', 12, '', 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Sazuki_Swift.jpeg'),
-(12, 'jeroen', 'van Oers', 17, 'female', 60, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL),
-(13, 'kaas', 'melk', 17, 'male', 60, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL),
-(14, 'good', 'good', 123, 'male', 123, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL);
+DROP TABLE IF EXISTS `yogaclasses`;
+CREATE TABLE `yogaclasses` (
+  `idyogaclasses` int(11) NOT NULL,
+  `essentialsYoga` varchar(30) NOT NULL,
+  `hathaYoga` varchar(30) NOT NULL,
+  `halfmoonYoga` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Indexen voor geëxporteerde tabellen
+-- Indexes for dumped tables
 --
 
 --
--- Indexen voor tabel `loginGebruiker`
+-- Indexes for table `achievements`
 --
-ALTER TABLE `loginGebruiker`
+ALTER TABLE `achievements`
+  ADD PRIMARY KEY (`idachievements`);
+
+--
+-- Indexes for table `danceclasses`
+--
+ALTER TABLE `danceclasses`
+  ADD PRIMARY KEY (`iddanceclasses`);
+
+--
+-- Indexes for table `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`idimages`);
+
+--
+-- Indexes for table `logingebruiker`
+--
+ALTER TABLE `logingebruiker`
   ADD PRIMARY KEY (`idloginGebruiker`);
 
 --
--- Indexen voor tabel `userprofile`
+-- Indexes for table `userprofile`
 --
 ALTER TABLE `userprofile`
   ADD PRIMARY KEY (`iduserprofile`);
 
 --
--- AUTO_INCREMENT voor geëxporteerde tabellen
+-- Indexes for table `yogaclasses`
+--
+ALTER TABLE `yogaclasses`
+  ADD PRIMARY KEY (`idyogaclasses`);
+
+--
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT voor een tabel `loginGebruiker`
+-- AUTO_INCREMENT for table `achievements`
 --
-ALTER TABLE `loginGebruiker`
-  MODIFY `idloginGebruiker` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
+ALTER TABLE `achievements`
+  MODIFY `idachievements` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT voor een tabel `userprofile`
+-- AUTO_INCREMENT for table `danceclasses`
+--
+ALTER TABLE `danceclasses`
+  MODIFY `iddanceclasses` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `images`
+--
+ALTER TABLE `images`
+  MODIFY `idimages` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `logingebruiker`
+--
+ALTER TABLE `logingebruiker`
+  MODIFY `idloginGebruiker` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+--
+-- AUTO_INCREMENT for table `userprofile`
 --
 ALTER TABLE `userprofile`
-  MODIFY `iduserprofile` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=15;
+  MODIFY `iduserprofile` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+--
+-- AUTO_INCREMENT for table `yogaclasses`
+--
+ALTER TABLE `yogaclasses`
+  MODIFY `idyogaclasses` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
